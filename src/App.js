4f661b69e1a459d6a7212c3c07506e61c7d84f6e
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-08-11 09:32:20 -0400

    Number

--- src/App.js
+++ src/App.js
@@ -43,14 +43,14 @@ function App() {
 
   const sustractFunction = async (id) => {
     try {
-        const selectedProduct = artworks.filter(e => e.product_id === id);
+        const selectedProduct = artworks.filter(e => e.product_id === Number(id));
         const body = {
             "user_id": user.user_id,
             "product_id": selectedProduct[0].product_id
         };
         await axios.put(urlServer+"/cart/sustract", body);
 
-        const artwork_index = artworks.findIndex((element) => element.product_id === id);
+        const artwork_index = artworks.findIndex((element) => element.product_id === Number(id));
         artworks[artwork_index].amount = artworks[artwork_index].amount - 1;
         setArtworks([...artworks]);
         setNavTotal(updatingNavTotal);
@@ -63,7 +63,7 @@ function App() {
 };
 const addFunction = async (id, counter = null) => {
     try {
-        const selectedProduct = artworks.filter(e => e.product_id === id);
+        const selectedProduct = artworks.filter(e => e.product_id === Number(id));
         const body = {
             "user_id": user.user_id,
             "product_id": selectedProduct[0].product_id,
@@ -72,7 +72,7 @@ const addFunction = async (id, counter = null) => {
         };
         await axios.post(urlServer+"/cart", body);
 
-        const artwork_index = artworks.findIndex((element) => element.product_id === id);
+        const artwork_index = artworks.findIndex((element) => element.product_id === Number(id));
 
         if (counter === null) {
           artworks[artwork_index].amount = artworks[artwork_index].amount + 1;
